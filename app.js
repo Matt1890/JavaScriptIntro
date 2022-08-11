a7106a1e12064eee1cbf235170dade1f69b2ccaf
“matthewhasemeier” <mshweb22@gmail.com>
2022-08-11 12:24:49 -0700

    added next section

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const firstName = "Matt";
 let lastName = "Hasemeier";
 var age = 42;
 
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
 console .log (firstName);
 console .log (lastName);
 console .log (age);
